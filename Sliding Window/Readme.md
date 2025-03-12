d357ab8757e13de7fbeefc8d46f5493c0ced63cc
Suryoday Yadav <82751526+syadav48@users.noreply.github.com>
2025-03-12 23:29:05 +0530

    longestNiceSUbString solving

--- Sliding Window/Readme.md
+++ Sliding Window/Readme.md
@@ -1,13 +1,16 @@
-The Sliding Window Algorithm is a technique used to solve problems involving arrays or strings by maintaining a "window" (a subarray or substring) and sliding it over the data to find a solution. It is efficient because it avoids redundant calculations and reduces the time complexity to O(n).
+# The Sliding Window Algorithm#
+is a technique used to solve problems involving arrays or strings by maintaining a "window" (a subarray or substring) and sliding it over the data to find a solution. It is efficient because it avoids redundant calculations and reduces the time complexity to O(n).
 
 
-Fixed-Size Sliding Window:
+## Fixed-Size Sliding Window:
 
 The window size is fixed (k).
 
 We calculate the sum of the first window and then slide the window by adding the next element and removing the first element of the previous window.
 
-Variable-Size Sliding Window:
+## Variable-Size Sliding Window:
+
+We have to findout the max and min window width that satisfy the condition.
 
 The window size changes dynamically.
 
--- Sliding Window/longestNiceSubStr.js
+++ Sliding Window/longestNiceSubStr.js
@@ -1,16 +1,70 @@
-var longestNiceSubstring = function(s) {
-    const charSet = []
-    let maxLength = 0
-    let start = 0
-    for(let i = 0; i < s.length; i++){
-        while(charSet.includes(s[i])){
-            console.log(charSet, "cchh");
-            charSet.shift(s[start])
-            start++
-        }
-        charSet.push(s[i])
+var longestNiceSubstring = function (s) {
+  let maxLength = 0;
+  let start = 0;
+  let windowSum = ''
+  const isNice = (str) => {
+    console.log(str);
+    
+    if(str.length < 2) return false
+    const set = new Set(str);
+    for (const char of set) {
+      const charCode = char.charCodeAt();
+      if (
+        (charCode >= 97 && !set.has(String.fromCharCode(charCode - 32))) ||
+        (charCode < 97 && !set.has(String.fromCharCode(charCode + 32)))
+      ) {
+        return false;
+      }
     }
-    return charSet
+    return true;
+  };
+  for (let i = 0; i < s.length; i++) {
+    while(!isNice(windowSum)){
+       windowSum = windowSum.slice(start)
+       console.log(windowSum);
+       
+        start++
+    }
+    maxLength = Math.max(maxLength, i - start + 1)
+    windowSum += s[i]
+  }
+  return maxLength
+
 };
 
 console.log(longestNiceSubstring("YazaAay"));
+
+//abcACB
+//bB
+const isNice = (str) => {
+  const allCharCode = [...new Set(str.split("").map((e) => e.charCodeAt()))];
+  for (let i = 0; i < allCharCode.length; i++) {
+    if (allCharCode[i] < 97) {
+      if (!allCharCode.includes(allCharCode[i] + 32)) {
+        return false;
+      }
+    } else {
+      if (!allCharCode.includes(allCharCode[i] - 32)) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+//console.log(isNice('abcccccBBBBbbbbaaaaAACCCCACB'));
+
+const isNice2 = (str) => {
+  const set = new Set(str);
+  for (const char of set) {
+    const charCode = char.charCodeAt();
+    if (
+      (charCode >= 97 && !set.has(String.fromCharCode(charCode - 32))) ||
+      (charCode < 97 && !set.has(String.fromCharCode(charCode + 32)))
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+//console.log(isNice2("abcccccBBBBbbbbaaaaAACCCCACB"));
+//console.log(isNice2("abd"));
