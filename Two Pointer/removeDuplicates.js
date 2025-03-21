de35120176670fee3cc8be9281b0efa76fadf3fd
Suryoday Yadav <82751526+syadav48@users.noreply.github.com>
2025-03-21 16:32:07 +0530

    removing and finding

---
+++ Two Pointer/removeDuplicates.js
@@ -0,0 +1,9 @@
+var removeDuplicates = function(nums) {
+    let count = 0
+    for(let i = 0; i < nums.length; i++){
+        let count = 0
+
+    }
+};
+console.log(removeDuplicates([1,1,1,2,2,3]));
+//[1,1,2,2,3]
