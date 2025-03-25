5979d69c69b908d0118fcbcb66561978aa18028a
Suryoday Yadav <82751526+syadav48@users.noreply.github.com>
2025-03-25 20:24:44 +0530

    hashtable changes

---
+++ Sliding Window/numOfSubArr.js
@@ -0,0 +1,10 @@
+var numberOfSubarrays = function(nums, k) {
+    let start = 0
+    const isNice = (arr) => arr.every((e) => e % 2 !== 0)
+    for(let i = 0; i < k; i++){
+        console.log(i);
+        
+    }
+};
+console.log(numberOfSubarrays([1,1,2,1,1], 3));
+console.log(3 % 2);
